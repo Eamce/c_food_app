be4ba571730265a1c556f934a39b6b6f380d5da6
pjjy <pauljearicniones36@gmail.com>
2022-07-30 16:07:51 +0800

    update

--- app/src/main/java/com/example/c_food_app/Server.java
+++ app/src/main/java/com/example/c_food_app/Server.java
@@ -2,6 +2,6 @@ package com.example.c_food_app;
 
 public class Server {
     public static String address = "http://192.168.0.107/cfood/";
-//    public static String address =  "http://172.16.46.130/cfood/";
+//   public static String address =  "http://172.16.46.130/cfood/";
     public static String key="XoAxVBnw9PYHzHkTFBQdG0MFCLMdnGFf";
 }
