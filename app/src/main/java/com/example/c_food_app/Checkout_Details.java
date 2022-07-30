846726457be0d8058489272fabe8cbc650b49f11
pjjy <pauljearicniones36@gmail.com>
2022-07-30 16:37:53 +0800

    update

--- app/src/main/java/com/example/c_food_app/Checkout_Details.java
+++ app/src/main/java/com/example/c_food_app/Checkout_Details.java
@@ -3,13 +3,36 @@ package com.example.c_food_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Checkout_Details extends AppCompatActivity {
-
+        EditText fullname,address;
+        Button checkout_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_details);
 
+        checkout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(fullname.getText().toString().isEmpty() || address.getText().toString().isEmpty() ){
+                    Toast.makeText(Checkout_Details.this, "Please fill up information!", Toast.LENGTH_SHORT).show();
+                }else{
+
+                }
+            }
+        });
+
+
+    }
+
+    public void init(){
+        fullname = (EditText) findViewById(R.id.fullname);
+        address  = (EditText) findViewById(R.id.address);
+        checkout_btn = (Button) findViewById(R.id.checkout_btn);
     }
 }
