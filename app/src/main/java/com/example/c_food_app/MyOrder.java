7afb02ca1ad190348b421de647fd67fc83522818
pjjy <pauljearicniones36@gmail.com>
2022-08-02 13:56:27 +0800

    update

---
+++ app/src/main/java/com/example/c_food_app/MyOrder.java
@@ -0,0 +1,25 @@
+package com.example.c_food_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MyOrder extends AppCompatActivity {
+
+    TextView address;
+    ListView order_list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_order);
+    }
+
+    public void init(){
+        address = (TextView) findViewById(R.id.address);
+        order_list = (ListView) findViewById(R.id.order_list);
+
+    }
+}
